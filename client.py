6a2f2f426d01f7bf7f743a0e4d87d67d01d314d8
blue-terminal <marco.dandrea777@gmail.com>
2024-12-13 18:49:07 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+import time
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ('192.168.111.160', 1090)
+    print('ascolto  {} porta {}'.format(*server_address))
+    sock.bind(server_address)
+    print('attesa')
+    data, address = sock.recvfrom(4096)
+    print('sei coleggato  {} al indirizzo {}'.format(len(data), address))
+    print(data.decode())
+#time.sleep(1000)
+#sock.close()
+#shutdown /t 0
---
+++ servee.py
@@ -0,0 +1,13 @@
+import socket
+import os
+import time
+while True:
+    sockets = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    indirizzo = ('192.168.111.160', 1090)
+    utente=input("inserisci comando\n")
+    message = os.system(utente)
+    print(f'Invio del messaggio: "{message}" a {indirizzo}')   
+    sockets.sendto(utente.encode(), indirizzo)
+    time.sleep(10)
+    sockets.close()
+
